332589be60f4cd4a7abf6e369190bd1d93944929
Krishna <kvadakattu@deloitte.co.uk>
2017-11-03 00:38:03 +0000

    Finished.

--- nlp.py
+++ nlp.py
@@ -1,7 +1,6 @@
 import glob
 import spacy
-import ipdb
-from collections import Counter, OrderedDict
+from collections import Counter
 import pandas as pd
 
 docs = {}
@@ -19,12 +18,13 @@ for doc in docs.items():
 # find most common words across all documents
 word_freq = Counter(words)
 
+top_n = 20
 results = []
-for word in word_freq.most_common(10):
+for word in word_freq.most_common(top_n):
     for doc in docs.items():
         sentences = [sent for sent in doc[1].sents if sum([word[0] == token.lower_ for token in sent]) > 0]
         for s in sentences:
             results.append((' - '.join(map(str, word)), doc[0], s))
 
 df = pd.DataFrame.from_records(results, columns=('Word - Frequency', 'Document', 'Sentence'))
-df.to_excel('output.xlsx', index=False)
+df.to_excel('top_%s_results.xlsx' % top_n, index=False)
